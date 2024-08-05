8c17d45fa127cc64d77536b2ded9feb8746ee0c3
Bernardo Andrade <berdfandrade@gmail.com>
2024-08-05 00:51:03 -0300

    --

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 // src/pages/Home.tsx
 import React from 'react';
-import { Container, Image } from '@chakra-ui/react';
+import { Container, Image, Text } from '@chakra-ui/react';
 import Navbar from '../components/navbar/Navbar';
 import PostList from '../components/blogPost/postList';
 import BENNY from '../assets/benny.png'
@@ -9,7 +9,9 @@ const Home: React.FC = () => {
     return (
         <>
             <Navbar />
-            <Image mr={'auto'} ml={'auto'}boxSize={'200px'}src={BENNY}/>
+
+            <Image mr={'auto'} ml={'auto'} boxSize={'200px'} src={BENNY} />
+            <Text w={'400px'} mr={'auto'} ml={'auto'}>Falo sobre programação, comunismo, livro, sexo e ressentimento com amigos antigos</Text>
             <Container maxW="container.md" mt={1}>
                 <PostList />
             </Container>
